dd2214e144a9a24c5996ccc0e32cf26116d981e5
Myc0plasmus <jablonski.natan@gmail.com>
2023-04-24 14:20:15 +0200

    lesson 06 - task completed

---
+++ lesson-06/testing.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Sequence {
+public:
+    virtual int Length() = 0;
+};
+
+class FakeSequence : public Sequence {
+public:
+    int Length() override { return 100; }
+};
+
+class Gene {
+private:
+    Sequence* sequence; 
+    string name;
+
+public:
+    Gene(Sequence* sequence, const string& name) : sequence(sequence), name(name) {}
+
+    void JSON() {
+        cout << "{\n";
+        cout << "\t\"Name\": \"" << name << "\",\n";
+        cout << "\t\"length\": " << sequence->Length() <<endl;
+        cout << "}\n";
+    }
+};
+int main() {
+    FakeSequence x;
+    Gene gene(&x, "cytochrome c");
+
+    gene.JSON();
+
+    return 0;
+}
